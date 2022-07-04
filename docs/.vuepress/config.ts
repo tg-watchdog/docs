fc944e72da6e8969a2bf0e5f6fb6addf7ef8fd15
Astrian Zheng <happy.wheel6743@astrian.moe>
2022-07-04 12:29:37 +0800

    Change nav

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
         nav: [
           { text: "Help Documents", link: "/help/"},
           { text: "FAQ", link: "/faq"},
-          { text: "Changelogs", link: "/changelogs/"},
-          { text: "Handbook", link: "/handbook/"},
+          { text: "Changelogs (CN)", link: "/zh/changelogs/"},
+          { text: "Handbook (CN)", link: "/zh/handbook/"},
           {
             text: "More", 
             items: [

5e6c72e77bf753a2a66115f1f8e5add903d967a6
Astrian Zheng <happy.wheel6743@astrian.moe>
2022-08-22 10:44:49 +1000

    Add new changelogs

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
           { text: "Help Documents", link: "/help/"},
           { text: "FAQ", link: "/faq"},
           { text: "Changelogs", link: "/changelogs/"},
-          { text: "Handbook (CN)", link: "/zh/handbook/"},
+          { text: "Handbook", link: "/handbook/"},
           {
             text: "More", 
             items: [
